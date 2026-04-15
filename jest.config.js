84aaecb7a6f5c1e3e1039216df78041074799fee
Mark Chmarny <mark@chmarny.com>
2026-04-15 13:09:56 -0700

    Scaffold devtrace-action repo with package.json, tsconfig, action.yml, CI

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
+  coverageThreshold: {
+    global: { branches: 80, functions: 80, lines: 80, statements: 80 }
+  }
+}
